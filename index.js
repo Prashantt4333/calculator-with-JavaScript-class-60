c23c31ffd29ea11e6ee186fedc0d2df24a06b8fc
prashantt4333@gmail.com <prashantt4333@gmail.com>
2022-12-24 13:44:42 +0530

    Clear button

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var isOperatorButtonClicked = false;
 
 
 let r = document.querySelector('.myresult')
+let C = document.querySelector('.Clear')
+var clear_id = document.getElementById('clear_id');
 var leftValue;
 var rightValue;
 var operator = ' ';
 
+$(document).ready(function(){
+    $('#clear_id').click(function(){
+        $('.myresult').text('');
+    })
+});
 
 function clicked(element){
 
@@ -14,14 +21,14 @@ function clicked(element){
    
 
     switch(element.innerHTML) {
+
+        
         case '*':
              isOperatorButtonClicked = true;
             // code block
             leftValue = r.innerHTML
             operator = '*';
-            
-          
-            
+
             break;
         case '+':
             // code block
